e0182248764682b2e28d2630527430f3322ef1d3
Samuel Tavares <ls30759@salleurl.edu>
2016-04-26 18:12:33 +0200

    canco

---
+++ espotify_Client/src/model/Canco.java
@@ -0,0 +1,62 @@
+package model;
+
+import java.util.ArrayList;
+
+
+public class Canco {
+	String nom;
+	String genere;
+	String album;
+	String artista;
+	String path;
+	
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	String estrelles;
+	int reproduccions;
+	
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getGenere() {
+		return genere;
+	}
+	public void setGenere(String genere) {
+		this.genere = genere;
+	}
+	public String getAlbum() {
+		return album;
+	}
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+	public String getArtista() {
+		return artista;
+	}
+	public void setArtista(String artista) {
+		this.artista = artista;
+	}
+	public String getEstrelles() {
+		return estrelles;
+	}
+	public void setEstrelles(String string) {
+		this.estrelles = string;
+	}
+	public int getReproduccions() {
+		return reproduccions;
+	}
+	public void setReproduccions(int reproduccions) {
+		this.reproduccions = reproduccions;
+	}
+	
+	public Canco(){}
+
+}
