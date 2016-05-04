5c2d04ef95299df6e0c13b57d33ed96e88162ac4
Jorge Melguizo <jorgemt10@hotmail.com>
2016-05-04 12:35:48 +0200

    Added files via upload
    deleteSong (interface)

---
+++ espotify_servidor/src/controller/DeleteController.java
@@ -0,0 +1,82 @@
+package controller;
+
+import java.awt.Component;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
+import javax.swing.SwingUtilities;
+
+import view.FinestraServidor;
+import view.MainWindow;
+
+public class DeleteController implements MouseListener {
+	// VISTA
+	private JFrame jf;
+	private FinestraServidor fs;
+	
+	public DeleteController(JFrame jf, FinestraServidor fs){
+		this.jf = jf;
+		this.fs = fs;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		if (SwingUtilities.isRightMouseButton(e))
+        {
+            // han apretat el boto dret
+			
+			JPopupMenu popupMenu = new JPopupMenu();
+	        JMenuItem deleteItem = new JMenuItem("Delete");
+	       //afegim al menu
+	        popupMenu.add(deleteItem);	
+			
+			// agafem l'event source
+	        Component b=(Component)e.getSource();
+	        
+	        // afafem la localitzacio del punt en la pantalla
+	        Point p=b.getLocationOnScreen();
+	        
+	        //el mostrem
+	        popupMenu.show(b,e.getX(),e.getY());
+	       
+
+			
+          
+			
+			
+        }
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
