e9f59a2223f220271074964e5669a263d72b1f34
Gabriel Cammany <gabriel.cammany@gmail.com>
2017-11-26 23:43:06 +0100

    Cleaning up project

---
+++ espotify_server/src/view/ErrorLog.java
@@ -0,0 +1,20 @@
+package view;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class ErrorLog {
+	
+	public ErrorLog(){}
+	
+	public void errorInsertUser(){
+		/*JFrame frame= new JFrame();
+		JOptionPane.showMessageDialog(frame,
+			    "Usuari no disponible.",
+			    "Error",
+			    JOptionPane.ERROR_MESSAGE);*/
+		
+		//PESTAÑA DE ERROR!!
+	}
+
+}
